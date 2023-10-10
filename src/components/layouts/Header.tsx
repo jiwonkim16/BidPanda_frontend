24a165c9c7f02b13d96f9f581fbcf2c13b6c2091
Kim Jiwon <stayby16@kakao.com>
2023-10-10 12:47:45 +0900

    #7🔥 상품등록 api 파일 생성

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,16 +1,16 @@
 import { Link } from "react-router-dom";
 
+export const nav =
+  "bg-gray-200 w-[390px] h-full rounded-t-[37px] rounded-b-none flex flex-row";
 function Header() {
   return (
     <>
-
-      <nav className="bg-gray-200 w-[390px] h-full rounded-t-[37px] rounded-b-none flex flex-row">
+      <nav className={nav}>
         <button className="text-gray-600 text-xl ml-[220px]">
           <Link to={"/"}>notification</Link>
         </button>
         <button className="text-gray-600 text-xl ml-2 ">
           <Link to={"/login"}>login</Link>
-
         </button>
       </nav>
     </>
