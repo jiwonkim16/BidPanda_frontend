15fba17dde889ce7ff9e139fc84f11a6dceff90c
Kim Jiwon <stayby16@kakao.com>
2023-10-17 18:25:56 +0900

    #12🔥 실시간 채팅 기능 (채팅 리스트, 채팅방 및 api 작성, 서버 연결 전)

--- src/apis/chat/ChattingList.ts
+++ src/apis/chat/ChattingList.ts
@@ -1,29 +1,27 @@
 import axios from "axios";
 
-interface IChattingList {
-  my: {
-    nickname: string;
-  };
-  opened: {
-    title: string;
-    item_id: number;
-    partner: string;
-    record_id: string;
-  }[];
-  not_opened: {
-    title: string;
-    item_id: number;
-  }[];
-}
+// interface IChattingList {
+//   my: {
+//     nickname: string;
+//   };
+//   opened: {
+//     title: string;
+//     item_id: number;
+//     partner: string;
+//     record_id: string;
+//   }[];
+//   not_opened: {
+//     title: string;
+//     item_id: number;
+//   }[];
+// }
 
 // /api/chat/rooms?user={seller|winner}
 // 채팅방 조회
-export const chattingList = async () => {
+export const chattingList = async (nickname: string) => {
   try {
     const response = await axios.get(
-      `${
-        import.meta.env.VITE_REACT_API_KEY
-      }/api/chat/rooms?user={seller|winner}`,
+      `${import.meta.env.VITE_REACT_API_KEY}/api/chat/rooms?user=${nickname}`,
       {
         headers: {
           Authorization: localStorage.getItem("authorization"),
@@ -39,7 +37,7 @@ export const chattingList = async () => {
 
 // /api/chat/room
 // 채팅방 입장
-export const enterChattingRoom = async (item_id) => {
+export const enterChattingRoom = async (item_id: any) => {
   try {
     const response = await axios.post(
       `${import.meta.env.VITE_REACT_API_KEY}/api/chat/room`,
