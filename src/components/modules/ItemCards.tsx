f6e9f5ffed186559b840005031de4405d8c0b4aa
Kim Jiwon <stayby16@kakao.com>
2023-10-21 17:33:51 +0900

    #81🔥 메인페이지 최고가 아이템 리스트 스와이퍼 구현

--- src/components/modules/ItemCards.tsx
+++ src/components/modules/ItemCards.tsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
 import CountdownTimer from "../../pages/auction-list/CountdownTimer";
 import { memo } from "react";
-import { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Scrollbar } from "swiper/modules";
+import "swiper/css/scrollbar";
+import "swiper/css";
+
 interface Items {
   auctionEndTime: string;
   auctionStatus: string;
@@ -18,74 +21,51 @@ interface TopItemProps {
   topItems: Items[];
 }
 
-const rowVariants = {
-  hidden: {
-    x: window.innerWidth,
-  },
-  visible: {
-    x: 0,
-  },
-  exit: {
-    x: -window.innerWidth - 500,
-  },
-};
-
 const ItemCards = ({ topItems }: TopItemProps) => {
-  const [index, setIndex] = useState(0);
-  const increaseIndex = () => setIndex((prev) => prev + 1);
-  console.log(index);
   return (
-    <div>
-      <div className="flex flex-row">
-        <AnimatePresence>
-          <motion.div
-            className="flex gap-[5px] flex-row overflow-x-scroll"
-            onClick={increaseIndex}
-            key={index}
-            variants={rowVariants}
-            initial="hidden"
-            animate="visible"
-            exit="exit"
-            transition={{ type: "tween", duration: 2.2 }}
-          >
-            {topItems.map((item: Items) => (
-              <div
-                key={item.id}
-                className="w-[170px] h-[240px] bg-white border border-gray-200 mt-1 mr-3 rounded-lg shadow"
-              >
-                <Link to={`/items/detail/${item.id}`}>
-                  <img
-                    className="p-2 rounded-lg w-[170px] h-[120px] object-cover"
-                    src={item.itemImages[0]}
-                    alt="product image"
-                  />
-                </Link>
-                <div className="px-3">
-                  <Link to={`/items/detail/${item.id}`}>
-                    <h5 className="text-lg font-bold text-gray-900">
-                      {item.title}
-                    </h5>
-                  </Link>
-                  <div className="flex">
-                    <span>
-                      <CountdownTimer
-                        endTime={item.auctionEndTime}
-                        bidCount={item.bidCount}
-                      />
-                    </span>
-                  </div>
-                  <div className="flex items-center justify-between">
-                    <span className=" font-semibold text-gray-900">
-                      {item.presentPrice}원
-                    </span>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </motion.div>
-        </AnimatePresence>
-      </div>
-    </div>
+    <Swiper
+      scrollbar={{
+        hide: true,
+      }}
+      slidesPerView={2.1}
+      centeredSlides={false}
+      modules={[Scrollbar]}
+      loop={true}
+      className="mySwiper w-full flex flex-row"
+    >
+      {topItems.map((item: Items) => (
+        <SwiperSlide
+          key={item.id}
+          className="w-[170px] h-[240px] bg-white border border-gray-200 mt-1 mr-3 rounded-lg shadow"
+        >
+          <Link to={`/items/detail/${item.id}`}>
+            <img
+              className="p-2 rounded-lg w-[170px] h-[120px] object-cover"
+              src={item.itemImages[0]}
+              alt="product image"
+            />
+          </Link>
+          <div className="px-3">
+            <Link to={`/items/detail/${item.id}`}>
+              <h5 className="text-lg font-bold text-gray-900">{item.title}</h5>
+            </Link>
+            <div className="flex">
+              <span>
+                <CountdownTimer
+                  endTime={item.auctionEndTime}
+                  bidCount={item.bidCount}
+                />
+              </span>
+            </div>
+            <div className="flex items-center justify-between">
+              <span className=" font-semibold text-gray-900">
+                {item.presentPrice}원
+              </span>
+            </div>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 };
 
