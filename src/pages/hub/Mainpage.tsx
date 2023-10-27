a6b7db513e7158f3e7f12cfd5f54642270b4c2af
Goya <gim.goya@gmail.com>
2023-10-27 13:51:50 +0900

    #16 🔥 배너 비디오 재생 문제 해결, 스플래쉬 페이지 기틀 개발

--- src/pages/hub/Mainpage.tsx
+++ src/pages/hub/Mainpage.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { getTopTenListApi } from "../../apis/auction-list/TopTenList";
 import ItemCards from "../../components/modules/ItemCards";
 import Categories from "../../components/modules/Categories";
+import Splash from "./Splash";
 
 export interface TopItemType {
   auctionEndTime: string;
@@ -22,8 +23,22 @@ export interface TopItemType {
 
 const Main = () => {
   const [topItems, setTopItems] = useState<TopItemType[]>([]);
+  const [showSplash, setShowSplash] = useState(true);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
 
   useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 1300);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.play();
+    }
+
     const fetchData = async () => {
       const data: TopItemType[] = await getTopTenListApi();
       if (data) {
@@ -31,22 +46,40 @@ const Main = () => {
       }
     };
     fetchData();
-  }, []);
+  }, [!showSplash]);
 
   return (
     <>
-      <div className="py-1">
-        <nav id="banner" className=" w-[390px] h-[210px] -mt-3">
-          <video src="/banner.mp4" className="max-w-md -mx-4 -mt-1" autoPlay />
-        </nav>
-        <div className="flex justify-center mt-[13px] flex-wrap">
-          <Categories />
-        </div>
-      </div>
-      <div className="ml-5 mt-5 font-bold">최고 낙찰가 상품 TOP 10</div>
-      <div className="flex items-center justify-start flex-wrap mt-1 ml-4 font-bold text-gray-700">
-        <ItemCards topItems={topItems} />
-      </div>
+      <>
+        {showSplash ? (
+          <Splash />
+        ) : (
+          <>
+            <div className="py-1">
+              <nav
+                id="banner"
+                className=" w-[390px] h-[210px] -mt-3 overflow-hidden"
+              >
+                <video
+                  src="/banner.mp4"
+                  ref={videoRef}
+                  className="max-w-md -mx-4 -mt-2"
+                  autoPlay
+                  muted
+                  playsInline
+                />
+              </nav>
+              <div className="flex justify-center mt-[13px] flex-wrap">
+                <Categories />
+              </div>
+            </div>
+            <div className="ml-5 mt-5 font-bold">최고 낙찰가 상품 TOP 10</div>
+            <div className="flex items-center justify-start flex-wrap mt-1 ml-4 font-bold text-gray-700">
+              <ItemCards topItems={topItems} />
+            </div>
+          </>
+        )}
+      </>
     </>
   );
 };
