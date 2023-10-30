332d2320a9bfdec5a8456856d944d6d4b7dc8664
Goya <gim.goya@gmail.com>
2023-10-30 17:00:07 +0900

    #39 🚑 알림 체크 시 UI 변환 구현, Footer div의 불투명 디테일 구현

--- src/apis/user-mylists/NotificationListApi.ts
+++ src/apis/user-mylists/NotificationListApi.ts
@@ -16,3 +16,22 @@ export const NotificationListApi = async () => {
     console.error(error);
   }
 };
+
+export const checkNotificationApi = async (notificationId: number) => {
+  try {
+    const res = await axios.put(
+      `${
+        import.meta.env.VITE_REACT_API_KEY
+      }/api/notification/read-notification/${notificationId}`,
+      notificationId,
+      {
+        headers: {
+          Authorization: localStorage.getItem("authorization"),
+        },
+      }
+    );
+    console.log(res);
+  } catch (error) {
+    console.error(error);
+  }
+};
