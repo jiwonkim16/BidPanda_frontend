a0d4422df46dce83c52be675d799a8c923c0051b
Kim Jiwon <stayby16@kakao.com>
2023-10-31 17:00:33 +0900

    #12🐛 메인페이지 top 10 카드 스타일 수정

--- src/components/modules/ItemCards.tsx
+++ src/components/modules/ItemCards.tsx
@@ -25,34 +25,37 @@ const ItemCards = ({ topItems }: TopItemProps) => {
       }}
       centeredSlides={false}
       modules={[Scrollbar, Autoplay]}
-      className="mySwiper w-full flex flex-row"
+      className="mySwiper w-[90%] flex flex-row"
     >
       {topItems.map((item: TopItemType) => (
         <SwiperSlide
           key={item.id}
-          className="bg-white border border-gray-200 mt-1 mr-3 rounded-xl"
+          className="mt-1 rounded-xl border-gray-200 border shadow-lg mr-3"
         >
-          <div className="w-[168px] h-[235px] shadow-md rounded-xl">
+          <div className="w-full h-[235px] shadow-md rounded-xl">
             <Link to={`/items/detail/${item.id}`}>
               <img
-                className="p-2 rounded-lg w-[170px] h-[140px] object-cover"
+                className="p-2 rounded-3xl w-[170px] h-[140px] object-cover"
                 src={item.itemImages[0]}
                 alt="product image"
               />
             </Link>
             <div className="px-3">
               <Link to={`/items/detail/${item.id}`}>
-                <h5 className="text-lg font-bold text-gray-900 mb-1 overflow-hidden text-ellipsis whitespace-nowrap">
+                <h5 className="text-lg font-bold text-gray-900 overflow-hidden text-ellipsis whitespace-nowrap">
                   {item.title}
                 </h5>
               </Link>
-              <div className="flex items-center justify-between">
+              <div className="text-sm text-gray-500 font-medium overflow-hidden text-ellipsis whitespace-nowrap">
+                <span>{item.content}</span>
+              </div>
+              <div className="flex items-center justify-between mt-1">
                 <span className=" font-semibold text-gray-900">
-                  {item.presentPrice.toLocaleString()}
+                  입찰가 : {item.presentPrice.toLocaleString()}
                 </span>
               </div>
               <div className="relative">
-                <div className="absolute -top-[200px] -right-[15px]">
+                <div className="absolute -top-[215px] -right-[20px]">
                   <ListTimer
                     endTime={item.auctionEndTime}
                     bidCount={item.bidCount}
