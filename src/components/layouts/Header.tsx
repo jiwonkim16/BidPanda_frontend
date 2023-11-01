3c0a8523f06b26e93a4a39e8a4714944b126a255
Goya <gim.goya@gmail.com>
2023-11-01 15:55:28 +0900

    #39 🚑 헤더 알림 아이콘에 실시간 기능 삽입

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
 import { isReadState } from "../../atoms/isReadState";
 import { getUserInfoApi } from "../../apis/user-mypage/UserInfoApi";
 import { toast } from "react-toastify";
@@ -12,7 +12,7 @@ import { toast } from "react-toastify";
 
 function Header() {
   const [profileImage, setProfileImage] = useState("");
-  const readData = useRecoilValue(isReadState);
+  const [readData, setReadDate] = useRecoilState(isReadState);
   const navigate = useNavigate();
   const removeToken = () => {
     localStorage.removeItem("authorization");
@@ -73,6 +73,7 @@ function Header() {
                 xmlns="http://www.w3.org/2000/svg"
                 height="1.5em"
                 viewBox="0 0 448 512"
+                onClick={() => setReadDate(false)}
               >
                 <path d="M224 0c-17.7 0-32 14.3-32 32V51.2C119 66 64 130.6 64 208v18.8c0 47-17.3 92.4-48.5 127.6l-7.4 8.3c-8.4 9.4-10.4 22.9-5.3 34.4S19.4 416 32 416H416c12.6 0 24-7.4 29.2-18.9s3.1-25-5.3-34.4l-7.4-8.3C401.3 319.2 384 273.9 384 226.8V208c0-77.4-55-142-128-156.8V32c0-17.7-14.3-32-32-32zm45.3 493.3c12-12 18.7-28.3 18.7-45.3H224 160c0 17 6.7 33.3 18.7 45.3s28.3 18.7 45.3 18.7s33.3-6.7 45.3-18.7z" />
               </svg>
@@ -106,6 +107,3 @@ function Header() {
   );
 }
 export default Header;
-
-// const navStyle =
-//   "bg-white w-[390px] h-[105%] flex py-1 justify-between border-b-2 rounded-b-[15px]";
