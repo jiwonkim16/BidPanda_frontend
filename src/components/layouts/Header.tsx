1a00ebd2e4703022014eefd23578e2f90d06ef4b
Goya <gim.goya@gmail.com>
2023-11-01 20:46:52 +0900

    #16 🚑 폰트 스타일링 2차 - 고야 파트

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -71,7 +71,7 @@ function Header() {
             <Link to={"/notification"}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                height="1.5em"
+                height="1.4em"
                 viewBox="0 0 448 512"
                 onClick={() => setReadDate(false)}
               >
@@ -79,7 +79,7 @@ function Header() {
               </svg>
               {readData === true ? (
                 <>
-                  <div className="rounded-full w-2.5 h-2.5 bg-red-500 absolute right-[97px] top-[12px]" />
+                  <div className="rounded-full w-2.5 h-2.5 bg-red-500 absolute right-[93px] top-[11px]" />
                 </>
               ) : (
                 <></>
@@ -88,14 +88,14 @@ function Header() {
             <button onClick={LogoutHandler}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                height="1.5em"
+                height="1.4em"
                 viewBox="0 0 512 512"
               >
                 <path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 192 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128zM160 96c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 32C43 32 0 75 0 128L0 384c0 53 43 96 96 96l64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l64 0z" />
               </svg>
             </button>
             <img
-              className="w-[33px] h-[33px] mr-3 cursor-pointer rounded-full object-cover shadow-md"
+              className="w-[35px] h-[35px] mr-2 mb-1 cursor-pointer rounded-full object-cover shadow-md"
               src={profileImage}
               alt="mypage&profile"
               onClick={() => navigate("/mypage")}
