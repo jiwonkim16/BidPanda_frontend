89db475b05bef1e7981b792cadce360f2710e43a
Goya <gim.goya@gmail.com>
2023-11-02 13:05:58 +0900

    #16 🚑 알림 아이콘 쪽 이상 현상 수정, 밸리데이션 메시지 폰트 수정

--- src/atoms/isReadState.ts
+++ src/atoms/isReadState.ts
@@ -4,12 +4,3 @@ export const isReadState = atom({
   key: "isReadState",
   default: false,
 });
-
-// interface AuctionStatus {
-//   [itemId: string]: boolean;
-// }
-
-// export const auctionStatusAtom = atom<AuctionStatus>({
-//   key: "auctionStatus",
-//   default: {},
-// });
